29caacffb305928b50e018af810bb99227fecf7e
DavidLesniak <dawid.lesniak01@outlook.com>
2025-06-03 22:55:37 +0200

    Dodanie zmiennych dla wielkości ekranu. Dodanie wyświetlania planszy.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
 
+SCREEN_WIDTH = 736
+SCREEN_HEIGHT = 736
+
 pygame.init()
 
-screen = pygame.display.set_mode((750, 750))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Monopoly")
+bg = pygame.image.load('board.png')
+
+def redrawGameWindow():
+    global bg
+
+    screen.blit(bg, (0,0))
+    pygame.display.update()
 
 run = True
 
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    redrawGameWindow()
 
 pygame.quit()
