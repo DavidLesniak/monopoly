106f15673bb6c8cb572c95622a2b660d9624ddec
Tomerson333 <coca.cola990@spoko.pl>
2025-06-16 20:12:17 +0200

    Dodanie zmiennych.

--- button.py
+++ button.py
@@ -1,9 +1,22 @@
 import pygame as pg
+from main import SCREEN_WIDTH, SCREEN_HEIGHT
 start_img = pg.image.load("grafika/start.png")
 start_hover_img = pg.image.load("grafika/start_hover.png")
+setup_img = pg.image.load("grafika/setup.png")
+setup_hover_img = pg.image.load("grafika/setup_hover.png")
 exit_img = pg.image.load("grafika/exit.png")
 exit_hover_img = pg.image.load("grafika/exit_hover.png")
 button_width = start_img.get_width()
+button_height = start_img.get_height()
+button_spacing = 30  
+menu_height = (button_height * 3) + (button_spacing * 2)
+
+start_x = (SCREEN_WIDTH - button_width) // 2
+start_y = (SCREEN_HEIGHT - menu_height) // 2
+setup_x = start_x
+setup_y = start_y + button_height + button_spacing
+exit_x = start_x
+exit_y = setup_y + button_height + button_spacing
 
 class Button:
     def __init__(self, x, y, image, hover_image, scale):
