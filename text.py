a688468adf1709efe6e5791c3f17facd7722f176
DavidLesniak <dawid.lesniak01@outlook.com>
2025-06-20 09:42:15 +0200

    Optymalizacja dziedziczenia

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Text:
-    def __init__(self, text, text_color, tlx, tly, font_size = 36, font_family = None):
+    def __init__(self, text, text_color, tlx, tly, font_size=36, font_family=None):
         self.text = str(text)
         self.text_color = text_color
         self.font_size = font_size
@@ -12,25 +12,21 @@ class Text:
         self.update()
 
     def update(self):
-        self.image = self.font.render(self.text, 1, self.text_color)
+        self.image = self.font.render(self.text, True, self.text_color)
         self.rect = self.image.get_rect()
         self.rect.topleft = self.tlx, self.tly
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
+
 class TextCenter(Text):
-    def __init__(self, text, text_color, cx, cy, font_size = 36, font_family = None):
-        self.text = str(text)
-        self.text_color = text_color
-        self.font_size = font_size
-        self.font_family = font_family
-        self.font = pg.font.SysFont(self.font_family, self.font_size)
+    def __init__(self, text, text_color, cx, cy, font_size=36, font_family=None):
         self.cx = cx
         self.cy = cy
-        self.update()
+        super().__init__(text, text_color, 0, 0, font_size, font_family)
 
     def update(self):
-        self.image = self.font.render(self.text, 1, self.text_color)
+        self.image = self.font.render(self.text, True, self.text_color)
         self.rect = self.image.get_rect()
         self.rect.center = self.cx, self.cy
